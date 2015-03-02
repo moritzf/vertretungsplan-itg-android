b0576fd394e3603ec3ee829e207d2465ea459014
moritzf <moritz.finsterwalder@gmail.com>
2015-03-02 22:14:20 +0100

    Create basic layout mock-up

--- app/src/main/java/app/vertretungsplan/itgdah2/de/vertretungsplanitg/MainActivity.java
+++ app/src/main/java/app/vertretungsplan/itgdah2/de/vertretungsplanitg/MainActivity.java
@@ -1,26 +1,36 @@
 package app.vertretungsplan.itgdah2.de.vertretungsplanitg;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        setContentView(R.layout.fragment_main);
+        String[] values = new String[] { "1   7d   Informatik   202   entfällt",
+                "1   8d   Sport   BS   entfällt",
+                "2   8d   Sport   BS   entfällt",
+                "2   7d   Informatik   202   Vertretung",
+                "7   8d   Informatik   202   " +
+                "entfällt", "1   7d   Informatik   202   entfällt",
+                "6   10a   Geografie   103   Vertretung",
+                "4   12   Sozialkunde   303   Vertretung",
+                "3   1g4   Geschichte   303   entfällt",
+                "4   8c   Deutsch   101   " +
+                "Vertretung" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 
 

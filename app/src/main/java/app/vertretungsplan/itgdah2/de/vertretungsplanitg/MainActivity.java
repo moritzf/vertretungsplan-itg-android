a5a424375701f4684909bf2e9daa0b29b358217a
glingoo <antoniooroz@icloud.com>
2015-03-20 19:13:18 +0100

    Hier die Veränderungen von heute -Antonio

--- app/src/main/java/app/vertretungsplan/itgdah2/de/vertretungsplanitg/MainActivity.java
+++ app/src/main/java/app/vertretungsplan/itgdah2/de/vertretungsplanitg/MainActivity.java
@@ -1,36 +1,48 @@
 package app.vertretungsplan.itgdah2.de.vertretungsplanitg;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
+
+    public String[] mTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_main);
-        String[] values = new String[] { "1   7d   Informatik   202   entfällt",
-                "1   8d   Sport   BS   entfällt",
-                "2   8d   Sport   BS   entfällt",
-                "2   7d   Informatik   202   Vertretung",
-                "7   8d   Informatik   202   " +
-                "entfällt", "1   7d   Informatik   202   entfällt",
-                "6   10a   Geografie   103   Vertretung",
-                "4   12   Sozialkunde   303   Vertretung",
-                "3   1g4   Geschichte   303   entfällt",
-                "4   8c   Deutsch   101   " +
-                "Vertretung" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        mTitles = getResources().getStringArray(R.array.drawer_titles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_activity);
+        mDrawerList = (ListView) findViewById(R.id.main_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.main_drawer_listitem, mTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.main_contentframe, new VertretungsplanFragment())
+                .commit();
     }
 
 
@@ -59,16 +71,47 @@ public class MainActivity extends ListActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class VertretungsplanFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        public VertretungsplanFragment() {
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState){
+            super.onActivityCreated(savedInstanceState);
+            Toolbar toolbar=(Toolbar) getActivity().findViewById(R.id.toolbar);
+            toolbar.inflateMenu(R.menu.menu_main);
+            toolbar.setTitle(getResources().getStringArray(R.array.drawer_titles)[0]);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.main_fragment_vertretungsplan, container, false);
             return rootView;
         }
+    }
+
+    private class DrawerItemClickListener implements android.widget.AdapterView.OnItemClickListener {
+
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Fragment fragment = new VertretungsplanFragment();
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_contentframe, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 }

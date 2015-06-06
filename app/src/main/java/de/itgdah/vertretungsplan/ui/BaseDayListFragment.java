edabdfc050de5815b2da5dcdafcc9a11ee475ca2
moritzf <moritz.finsterwalder@gmail.com>
2015-06-06 23:39:47 +0200

    Revert "fix bug where app doesn't open"
    This reverts commit ee7eeca07f9c010b72e829aede2340a77d2d34a0.

--- app/src/main/java/de/itgdah/vertretungsplan/ui/BaseDayListFragment.java
+++ app/src/main/java/de/itgdah/vertretungsplan/ui/BaseDayListFragment.java
@@ -150,7 +150,6 @@ public class BaseDayListFragment extends ListFragment implements
         // mMergeAdapter init
         mMergeAdapter = new MergeAdapter();
         mMergeAdapter.addView(mVertretungenHeader);
-        mMergeAdapter.setActive(mVertretungenHeader, false);
         mMergeAdapter.addAdapter(mVertretungenAdapter);
         mMergeAdapter.addView(mGeneralInfoHeader);
         mMergeAdapter.setActive(mGeneralInfoHeader, false);
@@ -170,12 +169,8 @@ public class BaseDayListFragment extends ListFragment implements
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 
         String dayId = getArguments().getString(DAY_ID_KEY);
-        String[] selectionArgs;
-        if (dayId != null) {
-            selectionArgs = new String[]{dayId};
-        } else {
-           selectionArgs = null;
-        }
+
+        String[] selectionArgs = new String[]{dayId};
         mSelection = Vertretungen.COLUMN_DAYS_KEY + " = ?";
 
         switch (id) {
@@ -204,9 +199,6 @@ public class BaseDayListFragment extends ListFragment implements
         int id = loader.getId();
         switch (id) {
             case VERTRETUNGEN_LOADER_ID: {
-                if (data.getCount() > 0) {
-                    mMergeAdapter.setActive(mVertretungenHeader, true);
-                }
                 mVertretungenAdapter.swapCursor
                         (data);
             }

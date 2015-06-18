e1cfe6159f57154a66a9363326b3ccecc7d70ebe
Moritz Finsterwalder <moritz.finsterwalder@gmail.com>
2015-06-18 21:19:20 +0200

    added screenshot

---
+++ app/src/main/java/de/itgdah/vertretungsplan/web/LoginConstants.java
@@ -0,0 +1,10 @@
+package de.itgdah.vertretungsplan.web;
+
+/**
+ * Interface that contains the login data for the vertretungsplan online access.
+ * This file MUSTN'T be added to git.
+ */
+interface LoginConstants {
+    String USERNAME = "itg";
+    String PASSWORD = "ps2015vp_app";
+}
